ff7de4cf05537897a8118638f5f713cb17485184
aniketg17 <aniketkg@gmail.com>
2020-05-23 22:39:27 +0530

    implemented helper function to find free space in the linked list

--- MemoryAllocators.c
+++ MemoryAllocators.c
@@ -6,8 +6,23 @@
 #include <unistd.h>
 
 pthread_mutex_t global_malloc_t;
+meta_header_t *head;
+meta_header_t *tail;
 
-void* malloc() {
+void* malloc(size_t bytes) {
+    if (bytes <= 0) {
+        return NULL;
+    }
+    return NULL;
+}
+
+meta_header_t* find_empty_space(size_t size) {
+    meta_header_t *traverse = head;
+    while (traverse != NULL) {
+        if (traverse->status == FREE && traverse->size >= size) {
+            return traverse;
+        }
+    }
     return NULL;
 }
 
@@ -24,5 +39,7 @@ void* calloc(size_t count, size_t size) {
 }
 
 int main() {
-    printf("%lu size_t %lu signed %lu unsigned\n", sizeof(size_t), sizeof(signed), sizeof(unsigned));
+    //printf("%lu size_t %lu signed %lu unsigned\n", sizeof(size_t), sizeof(signed), sizeof(unsigned));
+    
+    printf("%lu\n",sizeof(meta_header_t));
 }
